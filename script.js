407aaf95fd680a0c06c9b0d6914c44eedc2d1188
hibo ibrahim <hibo.ibrahim@gmail.com>
2023-10-26 15:28:28 +0000

    add api images to cards

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-const cards = document.querySelectorAll('.memory-card');
-let cardFlipped = false;
-let firstCard, secondCard;
+const accessKey = 'TG2TGsynwo0C05uWJ5lEOlHFTAX8B4nyFz7q5DLEogo';
+const apiUrl = 'https://api.unsplash.com/collections/9525226/photos/?per_page=20';
 
-function flipCard() {
-    this.classList.add('flip');
-  
-    if (!cardFlipped) {
-      // first click
-      hascardFlipped = true;
-      firstCard = this;
-    } else {
-      // second click
-      hasFlippedCard = false;
-      secondCard = this;
-  
-      console.log(firstCard.dataset.framework);
-      console.log(secondCard.dataset.framework);
-    }
+fetch(apiUrl, {
+  headers: {
+    'Authorization': `Client-ID ${accessKey}`
   }
-  
+})
+  .then(response => response.json())
+  .then(data => {
+    // Process the data and set background images for your cards
+    const cardElements = document.querySelectorAll('.card');
+    for (let i = 0; i < cardElements.length; i++) {
+      cardElements[i].style.backgroundImage = `url(${data[i].urls.regular})`;
+    }
+  })
+  .catch(error => console.error(error));
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+ 
+
